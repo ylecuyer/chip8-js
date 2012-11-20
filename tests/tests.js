4ea00c8ae89ee75312519a8cdd0e02e83d40617a
ylecuyer <yoann.lecuyer@gmail.com>
2012-11-20 01:08:53 +0100

    3 Opcodes unit test added

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,15 @@
+test("Chip8 initialization", function() {
+
+	expect(2);
+
+	init_chip8();
+
+	equal(PC, 0x200, "Program Counter OK");
+	equal(SP, 0xF, "Stack Pointer OK");
+
+});
+
+
 test("OpCode 00E0 - CLS", function() {
 
 	//Fill screen with pixels
@@ -15,6 +27,23 @@ test("OpCode 00E0 - CLS", function() {
 
 	ok(clean, "Screen cleared successfully");
 
+});
+
+test("OpCode 00EE - RET", function() {
+
+	init_chip8();
+	
+	PC += 0x2;	
+	Op2nnn_call(0x202);//Call 0x202
+	
+	PC += 0x2;
+	Op6xkk_ld(0,1);//Set V0 = 1
+
+	PC += 0x2;	
+	Op00EE_ret();
+	
+	equal(PC, 0x202, "Program Counter OK");
+	equal(SP, 0xF, "Stack Pointer OK");
 });
 
 
