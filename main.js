52d1f994388dc5e8aa9a2c1556dad9fbe5cfec25
ylecuyer <yoann.lecuyer@gmail.com>
2012-11-22 00:23:26 +0100

    Some fixes

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var DEBUG = false;
 
-var ROM_buffer = null;
-var ROM = null;
-
 var MEM_buffer = new ArrayBuffer(4096);
 var MEM = new Uint8Array(MEM_buffer);
+
+var keyboard = new Array(16);
+
+var wait_for_keypress =  false;
+var register_for_keypress = null;
 
 var register = {
 
@@ -14,11 +16,7 @@ var register = {
 	I:  0,
 
 	V: new Array(16)
-
 }
-
-for (var i = 0; i < 16; i++)
-	register.V[i] = 0;
 
 var PC = 0;
 var SP = 0;
@@ -26,129 +24,7 @@ var SP = 0;
 var STACK_buffer = new ArrayBuffer(32);
 var STACK = new Uint16Array(STACK_buffer);
 
-var keyboard = new Array(16);
-for (var i = 0; i < 16; i++)
-	keyboard[i] = 0;
-
-var wait_for_keypress =  false;
-var register_for_keypress = null;
-
-//64*32 = 2048
 var display = new Array(2048);
-for (var i = 0; i < 64*32; i++)
-	display[i] = 0;
-
-//0
-MEM[0x000] = 0xF0; //****
-MEM[0x001] = 0x90; //*  *
-MEM[0x002] = 0x90; //*  *
-MEM[0x003] = 0x90; //*  *
-MEM[0x004] = 0xF0; //****
-
-//1
-MEM[0x005] = 0x20; //  * 
-MEM[0x006] = 0x60; // ** 
-MEM[0x007] = 0x20; //  * 
-MEM[0x008] = 0x20; //  * 
-MEM[0x009] = 0x70; // ***
-
-//2
-MEM[0x00A] = 0xF0; //****
-MEM[0x00B] = 0x10; //   *
-MEM[0x00C] = 0xF0; //****
-MEM[0x00D] = 0x80; //*   
-MEM[0x00E] = 0xF0; //****
-
-//3
-MEM[0x00F] = 0xF0; //****
-MEM[0x010] = 0x10; //   *
-MEM[0x011] = 0xF0; //****
-MEM[0x012] = 0x10; //   *
-MEM[0x013] = 0xF0; //****
-
-//4
-MEM[0x014] = 0x90; //*  *
-MEM[0x015] = 0x90; //*  *
-MEM[0x016] = 0xF0; //****
-MEM[0x017] = 0x10; //   *
-MEM[0x018] = 0x10; //   *
-
-//5
-MEM[0x019] = 0xF0; //****
-MEM[0x01A] = 0x80; //*   
-MEM[0x01B] = 0xF0; //****
-MEM[0x01C] = 0x10; //   *
-MEM[0x01D] = 0xF0; //****
-
-//6
-MEM[0x01E] = 0xF0; //****
-MEM[0x01F] = 0x80; //*   
-MEM[0x020] = 0xF0; //****
-MEM[0x021] = 0x90; //*  *
-MEM[0x022] = 0xF0; //****
-
-//7
-MEM[0x023] = 0xF0; //****
-MEM[0x024] = 0x10; //   *
-MEM[0x025] = 0x20; //  * 
-MEM[0x026] = 0x40; // *  
-MEM[0x027] = 0x40; // *  
-
-//8
-MEM[0x028] = 0xF0; //****
-MEM[0x029] = 0x90; //*  *
-MEM[0x02A] = 0xF0; //****
-MEM[0x02B] = 0x90; //*  *
-MEM[0x02C] = 0xF0; //****
-
-//9
-MEM[0x02D] = 0xF0; //****
-MEM[0x02E] = 0x90; //*  *
-MEM[0x02F] = 0xF0; //****
-MEM[0x030] = 0x10; //   *
-MEM[0x031] = 0xF0; //****
-
-//A
-MEM[0x032] = 0xF0; //****
-MEM[0x033] = 0x90; //*  *
-MEM[0x034] = 0xF0; //****
-MEM[0x035] = 0x90; //*  *
-MEM[0x036] = 0x90; //*  *
-
-//B
-MEM[0x037] = 0xE0; //*** 
-MEM[0x038] = 0x90; //*  *
-MEM[0x039] = 0xE0; //*** 
-MEM[0x03A] = 0x90; //*  *
-MEM[0x03B] = 0xE0; //*** 
-
-//C
-MEM[0x03C] = 0xF0; //****
-MEM[0x03D] = 0x80; //*   
-MEM[0x03E] = 0x80; //*   
-MEM[0x03F] = 0x80; //*   
-MEM[0x040] = 0xF0; //****
-
-//D 
-MEM[0x041] = 0xE0; //*** 
-MEM[0x042] = 0x90; //*  *
-MEM[0x043] = 0x90; //*  *
-MEM[0x044] = 0x90; //*  *
-MEM[0x045] = 0xE0; //*** 
-
-//E
-MEM[0x046] = 0xF0; //****
-MEM[0x047] = 0x80; //*   
-MEM[0x048] = 0xF0; //****
-MEM[0x049] = 0x80; //*   
-MEM[0x04A] = 0xF0; //****
-
-//F
-MEM[0x04B] = 0xF0; //****
-MEM[0x04C] = 0x80; //*   
-MEM[0x04D] = 0xF0; //****
-MEM[0x04E] = 0x80; //*   
-MEM[0x04F] = 0x80; //*   
 
 var fps60 = Math.floor(1000/60);
 
@@ -442,8 +318,8 @@ function init_chip8() {
 	}
 
 	//Clear Memory
-	for (var i = 0; i <= 0xFFF; i++)
-		MEM[i] = 0;
+	//for (var i = 0; i <= 0xFFF; i++)
+	//	MEM[i] = 0;
 
 
 	//Clear registers
@@ -483,10 +359,9 @@ function init() {
 	init_chip8();
 
 	//Copy ROM inside the memory
-	
-	for (var i = 0; i < ROM_buffer.byteLength; i += 0x1) {
-		MEM[i + 0x200] = ROM[i];
-	}
+	//for (var i = 0; i < ROM_buffer.byteLength; i += 0x1) {
+	//	MEM[i + 0x200] = ROM[i];
+	//}
 	
 
 	/*
@@ -823,199 +698,4 @@ function process_opcode_ex() {
 		cpu_process = setTimeout(process_opcode_ex, 1);
 
 }
-
-$(function() {
-
-	$('#screen')
-		.clearCanvas()
-		.drawRect({
-			fillStyle: "black",
-			x: 0,
-			y: 0,
-			width: 256,
-			height: 128,
-			fromCenter: false
-		});
-
-	$("#rom").change(function(eventObject) {
-
-		var file = eventObject.target.files[0];
-
-		var reader = new FileReader();
-
-		reader.onload = function() {
-
-			ROM_buffer = this.result;
-			ROM = new Uint8Array(ROM_buffer);
-
-			init();
-		};
-
-		reader.readAsArrayBuffer(file);
-	});	
-
-	$("#stop").click(function() {
-
-		clearTimeout(cpu_process);
-		clearTimeout(draw_process);
-		clearTimeout(dt_process);
-		clearTimeout(st_process);
-	});
-	
-	$(document).keydown(function(event) {
-
-		var mapped_key = null;
-
-		switch (event.keyCode) {
-
-			case 222:
-				mapped_key = 0x1;
-				break;
-
-			case 57:
-				mapped_key = 0x2;
-				break;
-
-			case 189:
-				mapped_key = 0x3;
-				break;
-
-			case 55:
-				mapped_key = 0xC;
-				break;
-
-			case 82:
-				mapped_key = 0x4;
-				break;
-
-			case 84:
-				mapped_key = 0x5;
-				break;
-
-			case 89:
-				mapped_key = 0x6;
-				break;
-
-			case 85:
-				mapped_key = 0xD;
-				break;
-
-			case 70:
-				mapped_key = 0x7;
-				break;
-
-			case 71:
-				mapped_key = 0x8;
-				break;
-
-			case 72:
-				mapped_key = 0x9;
-				break;
-
-			case 74:
-				mapped_key = 0xE;
-				break;
-
-			case 86:
-				mapped_key = 0xA;
-				break;
-
-			case 66:
-				mapped_key = 0x0;
-				break;
-
-			case 78:
-				mapped_key = 0xB;
-				break;
-
-			case 188:
-				mapped_key = 0xF;
-				break;
-		}
-	
-		if (mapped_key !== null) {
-
-			keyboard[mapped_key] = 1;
-			
-			if (wait_for_keypress) {
-				wait_for_keypress = false;
-				register.V[register_for_keypress] = mapped_key;
-				cpu_process = setTimeout(process_opcode_ex, 1);
-			}
-
-		}		
-
-	});
-
-	$(document).keyup(function(event) {
-
-		switch (event.keyCode) {
-
-			case 222:
-				keyboard[0x1] = 0;
-				break;
-
-			case 57:
-				keyboard[0x2] = 0;
-				break;
-
-			case 189:
-				keyboard[0x3] = 0;
-				break;
-
-			case 55:
-				keyboard[0xC] = 0;
-				break;
-
-			case 82:
-				keyboard[0x4] = 0;
-				break;
-
-			case 84:
-				keyboard[0x5] = 0;
-				break;
-
-			case 89:
-				keyboard[0x6] = 0;
-				break;
-
-			case 85:
-				keyboard[0xD] = 0;
-				break;
-
-			case 70:
-				keyboard[0x7] = 0;
-				break;
-
-			case 71:
-				keyboard[0x8] = 0;
-				break;
-
-			case 72:
-				keyboard[0x9] = 0;
-				break;
-
-			case 74:
-				keyboard[0xE] = 0;
-				break;
-
-			case 86:
-				keyboard[0xA] = 0;
-				break;
-
-			case 66:
-				keyboard[0x0] = 0;
-				break;
-
-			case 78:
-				keyboard[0xB] = 0;
-				break;
-
-			case 188:
-				keyboard[0xF] = 0;
-				break;
-		}
-
-	});
-});
 
--- ui.js
+++ ui.js
@@ -6,6 +6,201 @@ $(function() {
 			
 	});
 
+
+	$('#screen')
+		.clearCanvas()
+		.drawRect({
+			fillStyle: "black",
+			x: 0,
+			y: 0,
+			width: 256,
+			height: 128,
+			fromCenter: false
+		});
+
+	$("#rom").change(function(eventObject) {
+
+		var file = eventObject.target.files[0];
+
+		var reader = new FileReader();
+
+		reader.onload = function() {
+
+			ROM_buffer = this.result;
+			ROM = new Uint8Array(ROM_buffer);
+			
+			chip8_load_rom_into_mem(ROM);
+			
+			init();
+		};
+
+		reader.readAsArrayBuffer(file);
+	});	
+
+	$("#stop").click(function() {
+
+		clearTimeout(cpu_process);
+		clearTimeout(draw_process);
+		clearTimeout(dt_process);
+		clearTimeout(st_process);
+	});
+	
+	$(document).keydown(function(event) {
+
+		var mapped_key = null;
+
+		switch (event.keyCode) {
+
+			case 222:
+				mapped_key = 0x1;
+				break;
+
+			case 57:
+				mapped_key = 0x2;
+				break;
+
+			case 189:
+				mapped_key = 0x3;
+				break;
+
+			case 55:
+				mapped_key = 0xC;
+				break;
+
+			case 82:
+				mapped_key = 0x4;
+				break;
+
+			case 84:
+				mapped_key = 0x5;
+				break;
+
+			case 89:
+				mapped_key = 0x6;
+				break;
+
+			case 85:
+				mapped_key = 0xD;
+				break;
+
+			case 70:
+				mapped_key = 0x7;
+				break;
+
+			case 71:
+				mapped_key = 0x8;
+				break;
+
+			case 72:
+				mapped_key = 0x9;
+				break;
+
+			case 74:
+				mapped_key = 0xE;
+				break;
+
+			case 86:
+				mapped_key = 0xA;
+				break;
+
+			case 66:
+				mapped_key = 0x0;
+				break;
+
+			case 78:
+				mapped_key = 0xB;
+				break;
+
+			case 188:
+				mapped_key = 0xF;
+				break;
+		}
+	
+		if (mapped_key !== null) {
+
+			keyboard[mapped_key] = 1;
+			
+			if (wait_for_keypress) {
+				wait_for_keypress = false;
+				register.V[register_for_keypress] = mapped_key;
+				cpu_process = setTimeout(process_opcode_ex, 1);
+			}
+
+		}		
+
+	});
+
+	$(document).keyup(function(event) {
+
+		switch (event.keyCode) {
+
+			case 222:
+				keyboard[0x1] = 0;
+				break;
+
+			case 57:
+				keyboard[0x2] = 0;
+				break;
+
+			case 189:
+				keyboard[0x3] = 0;
+				break;
+
+			case 55:
+				keyboard[0xC] = 0;
+				break;
+
+			case 82:
+				keyboard[0x4] = 0;
+				break;
+
+			case 84:
+				keyboard[0x5] = 0;
+				break;
+
+			case 89:
+				keyboard[0x6] = 0;
+				break;
+
+			case 85:
+				keyboard[0xD] = 0;
+				break;
+
+			case 70:
+				keyboard[0x7] = 0;
+				break;
+
+			case 71:
+				keyboard[0x8] = 0;
+				break;
+
+			case 72:
+				keyboard[0x9] = 0;
+				break;
+
+			case 74:
+				keyboard[0xE] = 0;
+				break;
+
+			case 86:
+				keyboard[0xA] = 0;
+				break;
+
+			case 66:
+				keyboard[0x0] = 0;
+				break;
+
+			case 78:
+				keyboard[0xB] = 0;
+				break;
+
+			case 188:
+				keyboard[0xF] = 0;
+				break;
+		}
+
+	});
+
 });
 
 function set_controls(file, run, pause, reset, eject) {
