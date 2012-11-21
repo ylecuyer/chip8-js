7374c444a00624c5a11c747162a7192de79b8406
ylecuyer <yoann.lecuyer@gmail.com>
2012-11-21 21:11:44 +0100

    Described control states according to engine state

--- ui.js
+++ ui.js
@@ -4,7 +4,45 @@ $(function() {
 
 		$("#rom").trigger("click");
 			
-	}
-
+	});
 
 });
+
+function set_controls(file, run, pause, reset, eject) {
+
+	$("#load_file").attr("disabled", !file);
+	$("#run").attr("disabled", !run);	
+	$("#pause").attr("disabled", !pause);
+	$("#reset").attr("disabled", !reset);
+	$("#stop").attr("disabled", !eject);	
+
+}
+
+function on_rom_loaded() {
+	
+	set_controls(false, true, false, true, true);
+}
+
+function on_engine_launched() {
+	
+	set_controls(false, false, true, true, true);
+
+}
+
+function on_engine_paused() {
+
+	set_controls(false, true, false, true, true);
+
+}
+
+function on_engine_stoped() {
+
+	set_controls(false, true, false, false, true);
+
+}
+
+function on_rom_removed() {
+	
+	set_controls(true, false, false, false, false);
+
+}
